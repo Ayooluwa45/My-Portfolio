e8630d13832d9b50799042ffdeb08f8173c1762b
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-03-19 09:28:28 +0100

    Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import CTA from './CTA'
+import './header.css'
+import HeaderSocials from './HeaderSocials'
 
 function Header() {
   return (
-    <div>Header</div>
+  <header>
+    <div className='container header_container'>
+      <h5>Hello I am </h5>
+      <h1>EMMANUEL SANNI</h1>
+      <h5 className='text-light'>React Developer</h5>
+      <CTA />
+      <HeaderSocials/>
+    </div>
+ 
+  </header>
   )
 }
 
