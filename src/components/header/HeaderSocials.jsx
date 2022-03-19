94b990e6d2890dd501997424c195601bf2588a31
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-03-19 09:29:22 +0100

    HeaderSocials component added

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+
+function HeaderSocials() {
+  return (
+    <div className='headerSocials'>
+        <a href="https://LinkedIn.com" target='_blank' ><BsLinkedin/></a>
+        <a href="https://github.com" target='_blank'></a>
+        <a href="https://dribble.com" target='_blank'></a>
+
+    </div>
+  )
+}
+
+export default HeaderSocials
