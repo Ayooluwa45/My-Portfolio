098951b7be8a5ec9f94d9c9125937b429aa2400f
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-03-19 01:32:15 +0100

    This is a new commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonial from "./components/testimonials/Testimonial";
+import Services from "./components/services/Services";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <>  
+    <About/>
+  <Contact/>
+  <Experience/>
+  <Header/>
+  <Nav/>
+  <Portfolio/>
+  <Services/>
+  <Testimonial/>
+  <Footer/>
+  </>
+
+  );
+}
+
+export default App;
