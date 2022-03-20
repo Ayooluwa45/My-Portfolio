ea812e733de68196526eac6b52d257272d8b4948
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-03-20 16:50:55 +0100

    Updated Components

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,11 +1,119 @@
-import React from 'react'
+import React from "react";
+import{BsFillPatchCheckFill} from'react-icons/bs'
+import './Experience.css'
 
 function Experience() {
   return (
-   <section id='experience'>
+    <section id="experience">
+      <h5>What Skills I have</h5>
+      <h2>My Experience</h2>
 
-   </section>
-  )
-}
+      <div className="container experienceContainer">
+        <div className="experienceFrontend">
+            <h3>Frontend Development</h3>
+            
 
-export default Experience
+             <div className="experienceContent">
+              <article className="experienceDetail">
+                  <BsFillPatchCheckFill className="experienceIcons"/>
+                  <div>
+                  <h4>HTML</h4>
+                  <small className="text-light">Experienced</small>
+                  </div>
+              </article>
+
+              <article className="experienceDetail">
+                  <BsFillPatchCheckFill className="experienceIcons"/>
+                 <div>
+                 <h4>CSS</h4>
+                  <small className="text-light">Intermediate</small>
+                 </div>
+              </article>
+
+              <article className="experienceDetail">
+                  <BsFillPatchCheckFill className="experienceIcons"/>
+                  <div>
+                  <h4>Javscript</h4>
+                  <small className="text-light">Experienced</small>
+                  </div>
+              </article>
+
+              <article className="experienceDetail">
+                  <BsFillPatchCheckFill className="experienceIcons"/>
+                  <div>
+                  <h4>Tailwind</h4>
+                  <small className="text-light">Experienced</small>
+                  </div>
+              </article>
+
+              <article className="experienceDetail">
+                  <BsFillPatchCheckFill className="experienceIcons"/>
+               <div>
+               <h4>Bootstrap</h4>
+                  <small className="text-light">Experienced</small>
+               </div>
+              </article>
+
+              <article className="experienceDetail">
+                  <BsFillPatchCheckFill className="experienceIcons"/>
+                 <div>
+                 <h4>React</h4>
+                  <small className="text-light">Experienced</small>
+                 </div>
+              </article>
+            </div>
+        </div>
+        <div className="experienceGisAnalyst">
+        <h3>Gespatial Skills</h3>
+        <div className="experienceContent">
+         
+              <article className="experienceDetail">
+                  <BsFillPatchCheckFill className="experienceIcons"/>
+                  <div>
+                  <h4>ARCGIS</h4>
+                  <small className="text-light">Experienced</small>
+                  </div>
+              </article>
+
+              <article className="experienceDetail">
+                  <BsFillPatchCheckFill className="experienceIcons"/>
+                  <div>
+                  <h4>ARCGIS</h4>
+                  <small className="text-light">Experienced</small>
+                  </div>
+              </article>
+
+              <article className="experienceDetail">
+                  <BsFillPatchCheckFill className="experienceIcons"/>
+                  <div>
+                  <h4>ARCGIS</h4>
+                  <small className="text-light">Experienced</small>
+                  </div>
+              </article>
+
+              <article className="experienceDetail">
+                  <BsFillPatchCheckFill className="experienceIcons"/>
+                  <div>
+                  <h4>ARCGIS</h4>
+                  <small className="text-light">Experienced</small>
+                  </div>
+              </article>
+
+              <article className="experienceDetail">
+                  <BsFillPatchCheckFill className="experienceIcons"/>
+                  <div>
+                  <h4>ARCGIS</h4>
+                  <small className="text-light">Experienced</small>
+                  </div>
+              </article>
+
+            
+            </div>
+        </div>
+
+      </div>
+    </section>
+  );
+} 
+
+export default Experience;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,20 +1,53 @@
-import React from 'react'
-import {FaHome} from 'react-icons/fa'
-import {FaUser} from 'react-icons/fa'
-import {BiBook} from 'react-icons/bi'
-import {RiServiceLine} from 'react-icons/ri'
-import {RiContactsFill} from 'react-icons/ri'
+import { useState } from "react";
+import "./nav.css";
+import { FaHome } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { RiContactsFill } from "react-icons/ri";
 
 function Nav() {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
     <nav>
-      <a href="#home"><FaHome/></a>
-      <a href="#about"><FaUser/></a>
-      <a href="#experience"><BiBook/></a>
-      <a href="#services"><RiServiceLine/></a>
-      <a href="#contact"><RiContactsFill/></a>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <FaHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <FaUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <RiContactsFill />
+      </a>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
